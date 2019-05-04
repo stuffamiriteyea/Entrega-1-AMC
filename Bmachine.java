925a4175acc5a3628ae729cbc59da5d7b90ebc82
Fred <fredpicado@gmail.com>
2019-05-04 17:24:23 +0100

    Update Bmachine.java

--- Bmachine.java
+++ Bmachine.java
@@ -2,14 +2,13 @@ package classes;
 
 import java.lang.Math;
 
-
 public class Bmachine {
-	private double[] d;
+	private int[] d;
 	private double [] a;
 	private	double [] b;
 	private	double [][] w;
 	
-	public Bmachine(double [] d, double [] a, double [] b, double [][] w) {
+	public Bmachine(int [] d, double [] a, double [] b, double [][] w) {
 			super();
 			this.d = d;
 			this.a = a;
@@ -17,11 +16,13 @@ public class Bmachine {
 			this.w = w;
 		}
 	
-	public double[] getD() {
+	
+	
+	public int[] getD() {
 		return d;
 	}
 
-	public void setD(double[] d) {
+	public void setD(int[] d) {
 		this.d = d;
 	}
 
@@ -54,11 +55,11 @@ public class Bmachine {
 		return "[" + b + ", " + a + ", " + w + "]";
 		}
 	
-	public double energy(double[] v, double[] h) {				
+	public double energia(double[] v, double[] h) {				
 		return (-produto(a,v) - produto(b,h) - produto(ProdutoMatriz(w, v),h));			
 		}
 		
-		//- Implementar um método que calcule o produto interno de dois vetores dados:
+		//- Implementar um mï¿½todo que calcule o produto interno de dois vetores dados:
 	public double produto( double [] vec1, double [] vec2 ) {
 		   int linha = vec1.length;  
 		   double r=0;
@@ -68,7 +69,7 @@ public class Bmachine {
 		
 		}
 		
-		//- Implementar um método que calcule o produto interno de uma vetores dados:
+		//- Implementar um mï¿½todo que calcule o produto interno de uma vetores dados:
 	public double[] ProdutoMatriz(double[][] matriz, double[] vetor) {
 		    int linha = matriz.length;
 		    int coluna= matriz[0].length;
@@ -83,81 +84,63 @@ public class Bmachine {
 		        }
 		    return r; 
 		    }
-	
-	public double[] DecomporV(double [] d, int i) {
-		int n = d.length;
-		double[] r = new double[n];
+
 		
-		//double prod = d[0];
-		//for (int j=1; j<n;j++)
-		//	prod *= d[j];
-		//if (i < 0 || i > prod)
-		//	EXIT
+	public double[] DecomporV(int i) throws Exception {
+			int n = d.length;
+			double[] r = new double[n];
 			
-		for(int j = 1; j <= n; j++) {
-			r[n-j] = i % d[n-j];
-			i /= d[n-j];
-		}
-		return r;
-	}
-	
-	public double[] DecomporH (int i) {
-		int n = b.length;
-		double[] r = new double[n];
-	
-		//double prod = d[0];
-		//for (int j=1; j<n;j++)
-		//	prod *= 2;
-		//if (i < 0 || i > prod)
-		//	EXIT
-		
-		for(int j = 1; j <= n; j++) {
-			r[n-j] = i % 2;
-			i /= 2;
-		}
-		return r;
-	}
-	
-	public double constantZ() { //está a devolver 1, apenas para as outras funções funcionarem
-		return 1;
-	}
-	
-	public double prob(double [] v, double[] h) {
-		return (1/constantZ())*(Math.exp(-energy(v,h)));
-	}
-	
-	public double probm(double[]v) {
-		double r=0;
-		for (int i=0; i<Math.pow(2, b.length); i++) {
-			double h[]=DecomporH(i);
-			r+=prob(v,h);
-		}
-		return r;
-	}
-	
-	public int classify(double[]v) {
-		double vaux []= new double [v.length+1];
-		for (int i=0; i<v.length; i++) 
-			vaux[i]=v[i];
-		double p=0;
-		int c=0;
-		for (int j=0;j<d[-1]; j++) {
-			vaux[-1]=j;
-			double paux=probm(vaux);
-			if (paux>p) {
-				p=paux;
-				c=j;
+			double prod = d[0];
+			for (int j = 1; j < n;j++)
+				prod *= d[j];
+			if (i < 0 || i >= prod)
+				throw new Error("valor de i impossÃ­vel");
+				
+			for(int j = 1; j <= n; j++) {
+				r[n-j] = i % d[n-j];
+				i /= d[n-j];
 			}
-		
+			return r;
 		}
-		return c;
-	}
-
 	
-	public void Update(double [] Aa, double [] Bb, double [][] Ww) {
-		setA(Aa);
-		setB(Bb);
-		setW(Ww);
+	public double[] DecomporH (int i) throws Exception{
+			int n = b.length;
+			double[] r = new double[n];
+		
+			double prod = 2;
+			for (int j = 1; j < n;j++)
+				prod *= 2;
+			if (i < 0 || i >= prod)
+				throw new Error("valor de i impossÃ­vel");
+			
+			for(int j = 1; j <= n; j++) {
+				r[n-j] = i % 2;
+				i /= 2;
+			}
+			return r;
+		}
+	
+	public double ConstZ () throws Exception {
+			
+		double prod = d[0];
+		double Z = 0;
+		
+		for (int i = 1; i < d.length;i++)
+			prod *= d[i];
+		for(int i = 0; i < prod; i++) {
+			for(int j = 0; j < Math.pow(2, b.length); j++) {
+				Z += Math.exp(- energia( DecomporV(i), DecomporH(j)) );
+			}
+		}
+		return Z;
 	}
-
+	
+		public void Update(double [] Aa, double [] Bb, double [][] Ww) {
+			setA(Aa);
+			setB(Bb);
+			setW(Ww);
+		}
 }
+				
+		
+
